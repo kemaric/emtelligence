7ce497a55dadd420ae80050fbe7f3c39fd0e1607
Kemaric@gmail.com <Kemaric@gmail.com@3a642cbe-17dc-6d55-5f4a-a25977ce2d8b>
2014-01-18 06:24:32 +0000

    new actions

---
+++ trunk/EmTelligence/src/com/example/emtelligence/EmotionActivity.java
@@ -0,0 +1,15 @@
+package com.example.emtelligence;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EmotionActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.emotional_value);
+	}
+
+}
--- trunk/EmTelligence/src/com/example/emtelligence/MainActivity.java
+++ trunk/EmTelligence/src/com/example/emtelligence/MainActivity.java
@@ -2,14 +2,43 @@ package com.example.emtelligence;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
+	Context context;
+	private ImageButton recordB, trackB;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		context = this;
+		
+		recordB = (ImageButton)findViewById(R.id.recordButton);
+		trackB = (ImageButton)findViewById(R.id.trackButton);
+		
+		/*function for record button action to make journal entry*/
+		recordB.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent emotionActivity = new Intent(MainActivity.this, EmotionActivity.class);
+				startActivity(emotionActivity);
+			}
+		});
+		
+		//Taking you to the page of tracking your past journal entries
+		trackB.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	@Override
@@ -18,5 +47,5 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
 }
