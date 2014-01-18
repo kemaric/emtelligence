51e7cb441e1f548d9cccc90945bbde2a79ec52a5
Kemaric@gmail.com <Kemaric@gmail.com@3a642cbe-17dc-6d55-5f4a-a25977ce2d8b>
2014-01-18 17:14:09 +0000

    fixed tracking activity skeleton

--- trunk/EmTelligence/src/com/example/emtelligence/TrackingActivity.java
+++ trunk/EmTelligence/src/com/example/emtelligence/TrackingActivity.java
@@ -9,7 +9,7 @@ public class TrackingActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.journal_entry);
+		setContentView(R.layout.tracking_options);
 	}
 	
 }
