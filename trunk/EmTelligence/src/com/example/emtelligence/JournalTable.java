c511d298362b5982d1cb30f436433d375d2c4c3a
Kemaric@gmail.com <Kemaric@gmail.com@3a642cbe-17dc-6d55-5f4a-a25977ce2d8b>
2014-01-18 04:22:34 +0000

    table for the journal entires

---
+++ trunk/EmTelligence/src/com/example/emtelligence/JournalTable.java
@@ -0,0 +1,18 @@
+package com.example.emtelligence;
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+public class JournalTable extends Activity{
+
+	private TableLayout journalTable; 
+	ArrayList<JournalEntry> JournalList;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	
+}
