5a75f29ff3f256ed7b0bb549b7ca62ed699ad8a8
Kemaric@gmail.com <Kemaric@gmail.com@3a642cbe-17dc-6d55-5f4a-a25977ce2d8b>
2014-01-18 17:57:08 +0000

    Updated the transitions.

--- trunk/EmTelligence/src/com/example/emtelligence/TrackingActivity.java
+++ trunk/EmTelligence/src/com/example/emtelligence/TrackingActivity.java
@@ -1,15 +1,40 @@
 package com.example.emtelligence;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TrackingActivity extends Activity{
 
+	private Button journalB, reportB;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tracking_options);
+		
+		journalB = (Button)findViewById(R.id.journalButton);
+		reportB = (Button)findViewById(R.id.progressButton);
+	
+		journalB.setOnClickListener( new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent journalTable = new Intent(TrackingActivity.this,JournalTable.class);
+				startActivity(journalTable);
+			}
+		});
+		
+		reportB.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent graphActivity = new Intent(TrackingActivity.this,Graph.class);
+				startActivity(graphActivity);
+			}
+		});
 	}
 	
 }
